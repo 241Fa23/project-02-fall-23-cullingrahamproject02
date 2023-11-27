64b27a2b7368a17b525e222dcfedae9b7e53139d
aidencullin <s534169@nwmissouri.edu>
2023-11-27 11:44:01 -0600

    Selection Structure
    Is not right

--- src/project02startingfiles/Project02StartingFiles.java
+++ src/project02startingfiles/Project02StartingFiles.java
@@ -10,11 +10,22 @@ import java.util.*;
 public class Project02StartingFiles {
 
     public static void main(String[] args) {
+        String playerClass;
         System.out.println("Welcome to JavaQuest!");
         System.out.println("Stay alive and increase your score!");
         Scanner input = new Scanner(System.in);
         System.out.println("\nChoose your character!");
         System.out.println("{k} Knight || {h} Healer || {t} Thief || {w} Wizard");
+        playerClass = input.next();
+        if (playerClass == k) {
+            Character ch1 = new Knight();
+        } else if (playerClass == h) {
+            Character ch1 = new Healer();
+        } else if (playerClass == t) {
+            Character ch1 = new Thief();
+        } else if (playerClass == w) {
+            Character ch1 = new Wizard();
+        }
     }
 
 }
